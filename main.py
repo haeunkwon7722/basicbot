9a69e0dbae229670ff8c85529a1c9d9faad73a09
haeunkwon7722 <104139418+haeunkwon7722@users.noreply.github.com>
2022-04-21 20:02:00 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(status=discord.Status.idle,activity=discord.Game('록시야 도움말'))
+    print(str(len(bot.guilds)))
+    print('Hello World!')
+    
+@bot.event
+async def on_message(msg):
+    if msg.author.bot: return None
+    await bot.process_commands(msg)  #The bot won't respond the message that the bot send.
+    
+@bot.command()
+async def dice(ctx):
+    import random
+    dice = random.randint(1, 6)
+    await ctx.channel.send(dice)
+    
+bot.run('Your Token')
